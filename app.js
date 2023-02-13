2f2443deabbe9c2e068b15940a1f410567acaf3f
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-13 16:15:01 -0400

    Comparing parameter on route /posts/:postName with each element of posts array to find if there's a Match

--- app.js
+++ app.js
@@ -42,7 +42,11 @@ app.post("/compose", function(req, res) {
 });
 
 app.get("/posts/:postName", function(req, res) {
-  console.log(req.params.postName);
+  posts.forEach(post=> {
+    if (post.postTitle === req.params.postName) {
+      console.log("Match found!");
+    }
+  });
 });
 
 
